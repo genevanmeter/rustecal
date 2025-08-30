89a81c28228a0d217b86e81dd577f4c14f0e9343
Gene VanMeter <47541158+genevanmeter@users.noreply.github.com>
2025-08-30 14:45:22 -0400

    Send a subset of protobuf descriptors (#86)

--- rustecal-types-protobuf/src/lib.rs
+++ rustecal-types-protobuf/src/lib.rs
@@ -3,7 +3,7 @@
 //! Provides support for Protobuf message serialization with rustecal.
 
 use prost::Message;
-use prost_reflect::ReflectMessage;
+use prost_reflect::{FileDescriptor, ReflectMessage};
 use rustecal_core::types::DataTypeInfo;
 use rustecal_pubsub::typed_publisher::PublisherMessage;
 use rustecal_pubsub::typed_subscriber::SubscriberMessage;
@@ -34,14 +34,49 @@ where
     /// This includes:
     /// - `proto` as encoding
     /// - the Rust type name
-    /// - an optional descriptor (currently empty)
+    /// - an optional descriptor
     fn datatype() -> DataTypeInfo {
         let default_instance = T::default();
+        let instance_descriptor = default_instance.descriptor();
+        let type_name = instance_descriptor.full_name().to_string();
+
+        let mut descriptor_pool = prost_reflect::DescriptorPool::new();
+
+        // List of proto files for a specific protobuf message type
+        let mut proto_filenames = instance_descriptor
+            .parent_file_descriptor_proto()
+            .dependency
+            .clone();
+        // Add the main proto message file
+        proto_filenames.push(
+            instance_descriptor
+                .parent_file_descriptor_proto()
+                .name()
+                .to_string(),
+        );
+
+        // Filter the pool to the set of file decriptors needed
+        let file_descriptors: Vec<FileDescriptor> = instance_descriptor
+            .parent_pool()
+            .files()
+            .filter(|s| proto_filenames.contains(&s.name().to_string()))
+            .collect();
+
+        for proto_file in file_descriptors.iter() {
+            let mut file_descriptor_proto = proto_file.file_descriptor_proto().clone();
+            // Remove the source_code_info from the descriptor which add excess comments
+            // from original proto to the descriptor message that aren't needed
+            file_descriptor_proto.source_code_info = None;
+
+            descriptor_pool
+                .add_file_descriptor_proto(file_descriptor_proto)
+                .unwrap();
+        }
 
         DataTypeInfo {
             encoding: "proto".to_string(),
-            type_name: default_instance.descriptor().full_name().to_string(),
-            descriptor: default_instance.descriptor().parent_pool().encode_to_vec(), // descriptor injection planned
+            type_name,
+            descriptor: descriptor_pool.encode_to_vec(),
         }
     }
 
@@ -61,7 +96,8 @@ impl<T> PublisherMessage for ProtobufMessage<T>
 where
     T: Message + Default + IsProtobufType + ReflectMessage,
 {
-    /// Returns the same datatype information as [`SubscriberMessage`] implementation.
+    /// Returns the same datatype information as [`SubscriberMessage`]
+    /// implementation.
     fn datatype() -> DataTypeInfo {
         <ProtobufMessage<T> as SubscriberMessage>::datatype()
     }
@@ -69,7 +105,8 @@ where
     /// Encodes the message to a byte buffer.
     ///
     /// # Panics
-    /// Will panic if `prost::Message::encode` fails (should never panic for valid messages).
+    /// Will panic if `prost::Message::encode` fails (should never panic for
+    /// valid messages).
     fn to_bytes(&self) -> Arc<[u8]> {
         let mut buf = Vec::with_capacity(self.data.encoded_len());
         self.data
